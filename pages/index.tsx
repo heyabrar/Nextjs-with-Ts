a64460b6dc10d76e3ac4b8ac55261be9200c6a87
Abrar Khan <heyabrarkhan@gmail.com>
2023-03-21 12:46:48 +0530

    home page modifed

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,11 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-    <Navbar/>
-    <h1>Welcome to HomePage</h1>
+      <Navbar />
+      <div style={{textAlign : 'center'}}> 
+        <h1>Welcome to HomePage</h1>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Nextjs-logo.svg/1200px-Nextjs-logo.svg.png'  width='600px'/>
+      </div>
     </>
   )
 }
