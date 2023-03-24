82937fbc1baaddbb88fddcf60c5891b2bc42a52b
Abrar Khan <heyabrarkhan@gmail.com>
2023-03-24 12:08:32 +0530

    Ui developed

--- Components/Todo.tsx
+++ Components/Todo.tsx
@@ -1,34 +1,36 @@
 import { GetTodoData } from "@/Fetch/Fetch";
 import { useEffect, useState } from "react";
 import AddTodo from "./AddTodo";
-interface Todo { 
-    id : number,
-    task : string,
-    status : boolean
+import { ImSad, ImHappy } from 'react-icons/im'
+interface Todo {
+    id: number,
+    task: string,
+    status: boolean
 }
 export default function Todo() {
-    const [task,setTask] = useState <Todo[ ] | [ ]>([ ]);
+    const [task, setTask] = useState<Todo[] | []>([]);
 
-    const handleGetTasks = ( ) =>{
-        GetTodoData( ).then((res)=>{
+    const handleGetTasks = () => {
+        GetTodoData().then((res) => {
             console.log(res.data)
             setTask(res.data)
         })
     }
 
-    useEffect(( ) =>{
-        handleGetTasks( );
-    }, [ ]);
+    useEffect(() => {
+        handleGetTasks();
+    }, []);
 
     return (
         <>
             <div>
-                <AddTodo/>
-
+                <AddTodo />
                 <div>
-                    {task.map((e:Todo)=>{
-                        return <div key={e.id}>
+                    {task.map((e: Todo) => {
+                        return <div key={e.id} className='flex justify-between w-11/12 m-auto text-lg'>
                             <p>{e.task}</p>
+                            <p className={`${e.status ? 'text-green-600' : 'text-red-600'}`}>{e.task ? 'Incomplete' : 'Completed'}</p>
+                            <p className={`${e.status ? 'text-green-600' : 'text-red-600'}`}> {e.status ? <ImHappy/> : <ImSad />}</p>
                         </div>
                     })}
                 </div>
