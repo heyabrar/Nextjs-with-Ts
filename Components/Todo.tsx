3769d0b421e103d992bd6533ec7b74be958e744e
Abrar Khan <heyabrarkhan@gmail.com>
2023-03-24 11:43:00 +0530

    Todo Alert Added

---
+++ Components/Todo.tsx
@@ -0,0 +1,11 @@
+import AddTodo from "./AddTodo";
+
+export default function Todo() {
+    return (
+        <>
+            <div>
+                <AddTodo/>
+            </div>
+        </>
+    )
+}
--- pages/todoPage.tsx
+++ pages/todoPage.tsx
@@ -1,9 +1,11 @@
 import Navbar from "@/Components/Navbar"
+import Todo from "@/Components/Todo"
 export default function TodoPage ( ) {
     return (
         <>
         <Navbar/>
         <h1 className="text-center text-2xl mt-10">Welcome to Todo App</h1>
+        <Todo/>
         </>
     )
 }
